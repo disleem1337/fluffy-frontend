b82b95a6fe0286a20ce70787536e0c76a6e556e0
Disleem <disleem59@gmail.com>
2022-11-28 13:14:52 +0300

    Fix merge conflicts

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,7 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import GlobalStyles from "./styles/GlobalStyles";
-import {
-	createBrowserRouter,
-	RouterProvider,
-  } from "react-router-dom";
-import Login from "./pages/login";
 import App from "./App";
-
-const router = createBrowserRouter([
-	{
-	  path: "/",
-	  element:<App/>
-	},
-	{
-	  path: "/login",
-	  element:<Login/>
-	}
-  ]);
 
 const container = document.getElementById("root");
 const root = createRoot(container!);
@@ -25,6 +9,6 @@ const root = createRoot(container!);
 root.render(
 	<React.StrictMode>
 		<GlobalStyles />
-		<RouterProvider router={router} />
+		<App />
 	</React.StrictMode>
 );
